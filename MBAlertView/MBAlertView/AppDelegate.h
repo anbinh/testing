cad8c5b7ea33a8339d7a0dfd43e1059dd3f47cf6
binhdna <binhdna@gmail.com>
2013-04-06 10:44:52 +0700

    add few projects

---
+++ MBAlertView/MBAlertView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MBAlertView
+//
+//  Created by Binh on 4/6/13.
+//  Copyright (c) 2013 BinhDNA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MBAlertView/MBAlertView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MBAlertView
+//
+//  Created by Binh on 4/6/13.
+//  Copyright (c) 2013 BinhDNA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
